32bdfb9428e29a4994e676ae4bd655e301cc28b8
Omkar-kamble82 <omkarsk82@gmail.com>
2026-03-12 16:28:28 +0530

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <Button className="bg-blue-500 text-white hover:bg-blue-600">
+      Hello World
+    </Button>
+  );
+}
