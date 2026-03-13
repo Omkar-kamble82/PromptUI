5301e129873235d2dcdecdf17c3338b81971f02b
Omkar-kamble82 <omkarsk82@gmail.com>
2026-03-13 09:24:28 +0530

    db setup

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <Button className="bg-blue-500 text-white hover:bg-blue-600">
-      Hello World
-    </Button>
+    <nav className="flex items-center justify-between px-4 py-2 shadow-2xl">
+      <Image src={'/logo.png'} alt="Logo" width={180} height={50} />
+    </nav>
   );
 }
